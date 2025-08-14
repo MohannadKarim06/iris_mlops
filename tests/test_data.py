b1ef79aca522dfe0f7717dbd94b96a7cc25c0c2e
Mohannad Karim <mohnnad.business@gmail.com>
2025-08-14 18:51:22 +0300

    done4

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 import pandas as pd
 from src.data_validation import validate_data_schema, validate_data_quality
